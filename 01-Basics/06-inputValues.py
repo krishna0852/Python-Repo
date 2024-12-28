3eed5d2ba3b92ea2caaaab519dc959e0ee4a454d
krishna0852 <krishnatejapalla1@gmail.com>
2024-12-28 23:34:28 +0530

    taking input values from console

---
+++ 01-Basics/06-inputValues.py
@@ -0,0 +1,18 @@
+# num1=input("enter a num1 value")
+# num2=input("enter a num2 value")
+
+# By default input() function takes arugments from console in string type, we need to typecast it
+
+num1= int(input("Enter a num1 value"))
+num2= int(input("Enter a num2 value"))
+#print(num1,num2)
+
+print("Entered values are num1=%d,num2=%d" %(num1,num2))
+
+
+eid=int(input("Enter a employee id:"))
+ename=input("Enter a employee name:")
+esal=float(input("Enter a employee salary:"))
+
+print("Employee Details are: eid=%d,ename=%s,esal=%f" %(eid,ename,esal))
+
