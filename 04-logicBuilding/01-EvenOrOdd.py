195b1493ab62038bd69fb9bb9f0464282bd22b25
krishna0852 <krishnatejapalla1@gmail.com>
2025-01-01 22:58:43 +0530

    code to check even or odd

---
+++ 04-logicBuilding/01-EvenOrOdd.py
@@ -0,0 +1,6 @@
+number=int(input("Enter a number to check if it's even or not"))
+
+if (number % 2 ==0):
+    print("The number %d is an even. " %(number))
+else:
+    print("The  number %d is an odd."  %(number))
